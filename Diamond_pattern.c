17ab0bb32a7748b754f2ecb44b07b078fd572834
Sabarinathan <sabarinathanr.tech@gmail.com>
2026-01-26 18:56:02 +0530

    Add diamond pattern printing program
    Implement a program to print a diamond pattern based on user input.

---
+++ Diamond_pattern.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int k=1;k<=n-i;k++){
+            printf(" ");
+        }
+        for(int j=1;j<=2*i-1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(int i=n-1;i>=1;i--){
+        for(int k=n-i;k>=1;k--){
+            printf(" ");
+        }
+        for(int j=1;j<=2*i-1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
