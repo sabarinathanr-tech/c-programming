2f251238539470c5425d4eaff1fa03c7b1ad0245
Sabarinathan <sabarinathanr.tech@gmail.com>
2026-01-29 12:51:02 +0530

    Add k-th max element finder in an array
    Implement a program to find the k-th maximum element in an array.

---
+++ Find_kth_max_element_in_an_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	int n, k;
+	scanf("%d", &n);
+
+	int arr[n];
+	for(int i=0; i<n; i++) {
+		scanf("%d",&arr[i]);
+	}
+	scanf("%d",&k);
+	for(int i=0; i<n-1; i++) {
+		for(int j=0; j<n-i-1; j++) {
+			if(arr[j] > arr[j+1]) {
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+
+	printf("%d", arr[n-k]);
+}
