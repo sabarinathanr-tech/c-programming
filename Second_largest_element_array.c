39626b4dfcb5902aa5a4b2165ad3e08093e7ac0c
Sabarinathan <sabarinathanr.tech@gmail.com>
2026-02-01 21:51:32 +0530

    Create Second_largest_element_array.c

---
+++ Second_largest_element_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int arr[n];
+
+	int max=-1,secmax=-1;
+
+	for(int i=0; i<n; i++) {
+		scanf("%d",&arr[i]);
+		if(arr[i]>max) {
+			secmax=max;
+			max=arr[i];
+		}
+		else if(arr[i]>max&&arr[i]<secmax) {
+			secmax=arr[i];
+		}
+
+	}
+	printf("%d",secmax);
+
+}
