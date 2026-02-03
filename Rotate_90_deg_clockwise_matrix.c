e44f911e2cda25fb13043853d91ae715d6530148
Sabarinathan <sabarinathanr.tech@gmail.com>
2026-02-03 09:24:25 +0530

    Create Rotate_90_deg_clockwise_matrix.c

---
+++ Rotate_90_deg_clockwise_matrix.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+	int row,col;
+	scanf("%d %d",&row,&col);
+	int arr[row][col];
+	for(int i=0; i<row; i++) {
+		for(int j=0; j<col; j++) {
+			scanf("%d",&arr[i][j]);
+		}
+	}
+	int t[col][row];
+	for(int i=0; i<row; i++) {
+		for(int j=0; j<col; j++) {
+			t[j][row-i-1]=arr[i][j];
+		}
+	}
+	printf("\n");
+	for(int i=0; i<row; i++) {
+		for(int j=0; j<col; j++) {
+			printf("%d ",t[i][j]);
+		}
+		printf("\n");
+
+	}
+	return 0;
+}
