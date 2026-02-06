06ff7b22795b149e0d781c2972343ea89e729818
Sabarinathan <sabarinathanr.tech@gmail.com>
2026-02-06 06:42:14 +0530

    Update Matrix_multiplication.c

--- Matrix_multiplication.c
+++ Matrix_multiplication.c
@@ -2,44 +2,45 @@
 
 int main()
 {
-	int row,col;
-	scanf("%d %d",&row,&col);
+    int r1,c1;
+    scanf("%d %d",&r1,&c1);
+    int arr[r1][c1];
 
-	int a[row][col];
+    for(int i=0;i<r1;i++){
+        for(int j=0;j<c1;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
 
-	for(int i=0; i<row; i++)
-		for(int j=0; j<col; j++)
-			scanf("%d",&a[i][j]);
+    int r2,c2;
+    scanf("%d %d",&r2,&c2);
+    int a[r2][c2];
 
-	int row1,col1;
-	scanf("%d %d",&row1,&col1);
+    if(c1 != r2) return 0;
 
-	if(col != row1) {
-		return 0;
-	}
+    for(int i=0;i<r2;i++){
+        for(int j=0;j<c2;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
 
-	int b[row1][col1];
+    int mul[r1][c2];
 
-	for(int i=0; i<row1; i++)
-		for(int j=0; j<col1; j++)
-			scanf("%d",&b[i][j]);
+    for(int i=0;i<r1;i++){
+        for(int j=0;j<c2;j++){
+            mul[i][j] = 0;
+            for(int k=0;k<c1;k++){  
+                mul[i][j] += arr[i][k] * a[k][j];   
+            }
+        }
+    }
 
-	int mul[row][col1];
+    for(int i=0;i<r1;i++){
+        for(int j=0;j<c2;j++){
+            printf("%d ",mul[i][j]);
+        }
+        printf("\n");
+    }
 
-	for(int i=0; i<row; i++) {
-		for(int j=0; j<col1; j++) {
-			mul[i][j]=0;
-			for(int k=0; k<col; k++) {
-				mul[i][j]+=a[i][k]*b[k][j];
-			}
-		}
-	}
-
-	for(int i=0; i<row; i++) {
-		for(int j=0; j<col1; j++)
-			printf("%d ",mul[i][j]);
-		printf("\n");
-	}
-
-	return 0;
+    return 0;
 }
