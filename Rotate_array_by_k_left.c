ad460c0a322ba3dff1c142071bee9850a21714d0
Sabarinathan <sabarinathanr.tech@gmail.com>
2026-02-12 19:01:03 +0530

    Implement left rotation of array by k positions

--- Rotate_array_by_k_left.c
+++ Rotate_array_by_k_left.c
@@ -1,35 +1,29 @@
+
 #include <stdio.h>
-
-void reverse(int* arr,int start,int end) {
-	while(start<end) {
-		int temp=arr[start];
-		arr[start]=arr[end];
-		arr[end]=temp;
-		start++;
-		end--;
-	}
-}
-
-void rotate(int* arr,int n,int k) {
-
-	reverse(arr,0,k-1);
-	reverse(arr,k,n-1);
-	reverse(arr,0,n-1);
-  
-}
 
 int main()
 {
-	int n,k;
+	int n;
 	scanf("%d",&n);
 	int arr[n];
 	for(int i=0; i<n; i++) {
 		scanf("%d",&arr[i]);
 	}
+
+	int k;
 	scanf("%d",&k);
-	rotate(arr,n,k);
+
+	for(int i=0; i<k; i++) {
+		int first=arr[0];
+		for(int j=0; j<n-1; j++) {
+			arr[j]=arr[j+1];
+		}
+		arr[n-1]=first;
+	}
+
 	for(int i=0; i<n; i++) {
 		printf("%d ",arr[i]);
 	}
+
 	return 0;
 }
