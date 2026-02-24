9ba2e912192476347b94e931ffe2883cdbf0ba9f
Sabarinathan <sabarinathanr.tech@gmail.com>
2026-02-24 22:51:05 +0530

    Implement single number finder in C

---
+++ single_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        ans ^= arr[i];
+    }
+
+    printf("%d", ans);
+
+    return 0;
+}
